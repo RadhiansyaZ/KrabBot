713ede15aff6873a6e95b605643f44aecb6c7810
Radhiansya Zain Antriksa Putra <radhi.rz666@gmail.com>
2020-09-11 03:58:17 +0700

    Now command using Collection rather than if-else chain

--- main.js
+++ main.js
@@ -8,26 +8,25 @@ client.once('ready', () => {
 });
 
 client.commands = new Discord.Collection(); // Storing command list available for the bot.
-// TODO: fs read and filter the js file
-// TODO: append to the Collection
+let commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));  
+// Array with string of <command>.js
+
+for (let file of commandFiles) { // Creating Collection of Command.
+    let command = require(`./commands/${file}`)
+    client.commands.set(command.name, command)
+
+}
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/); //RegEx to prevent bugs
     const command = args.shift().toLowerCase();
-        
-    // if (command === 'ping') {
-    //     message.channel.send('Pong!');
-    // } else if (command === 'beep') {
-    //     message.channel.send('Boop.');
-    // } else if (command === 'commands'){
-    //     message.channel.send('This command currently unavailable, sorry :(');
-    // } else {
-    //     message.channel.send('Command not found. Open list of commands with \\commands');
-    // }
 
-    if(!client.commands.has(command)) return;
+    if(!client.commands.has(command)) {
+        message.reply('Command not found.');
+        return;
+    }
     try {
         client.commands.get(command).execute(message, args);
     } catch (error) {
